66e3e88407bb2fc923ae55d611ffe75d05333c82
jnmayo <30914728+jnmayo@users.noreply.github.com>
2019-10-07 16:37:57 -0700

    Add files via upload

---
+++ Adjust.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adjust : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 300, 140, 30),"Health up"))
+        {
+            GameControl.control.health += 10;
+        }
+        if (GUI.Button(new Rect(10, 340, 140, 30), "Health down"))
+        {
+            GameControl.control.health -= 10;
+        }
+        if (GUI.Button(new Rect(10, 380, 140, 30), "Experience up"))
+        {
+            GameControl.control.experience += 10;
+        }
+        if (GUI.Button(new Rect(10, 420, 140, 30), "Experience down"))
+        {
+            GameControl.control.experience -= 10;
+        }
+
+        if (GUI.Button(new Rect(10, 460, 140, 30), "Save"))
+        {
+            GameControl.control.Save();
+        }
+        if (GUI.Button(new Rect(10, 500, 140, 30), "Load"))
+        {
+            GameControl.control.Load();
+        }
+    }
+}
